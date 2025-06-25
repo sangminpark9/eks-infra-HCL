40aa240e72a5d7278835e15c266b29f102ae7b23
sangminpark9 <52732279+sangminpark9@users.noreply.github.com>
2025-06-25 11:22:05 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
