c6c6cfdaae6d76063e0327f67b06bf7e014e16e4
sangminpark9 <renopark05@gmail.com>
2025-08-07 15:30:37 +0900

    change k8s version

--- eks/eks.tf
+++ eks/eks.tf
@@ -2,7 +2,7 @@
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn  # IAM 모듈에서 전달된 클러스터 역할 ARN
-
+  version = var.cluster_version
   vpc_config {
     subnet_ids = var.subnet_ids     # VPC 모듈에서 전달된 서브넷 리스트
     # (추가로 보안 그룹을 지정할 수 있음)
@@ -12,6 +12,7 @@ resource "aws_eks_cluster" "main" {
 # (선택사항) EKS 노드 그룹: 워커노드 역할 ARN 사용
 resource "aws_eks_node_group" "workers" {
   cluster_name    = aws_eks_cluster.main.name
+  version = var.cluster_version
   node_group_name = var.node_group_name
   node_role_arn   = var.worker_role_arn  # IAM 모듈에서 전달된 워커 역할 ARN
   subnet_ids      = var.subnet_ids
--- eks/variables.tf
+++ eks/variables.tf
@@ -6,6 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
+  default     = "1.30"
 }
 
 variable "subnet_ids" {
